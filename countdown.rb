9971ceabcb6bc82edc4abbefe3eb487cdd1be646
Amy Tu <siamesebb@gmail.com>
2018-05-25 03:34:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(number)
-  counter = number
   while number > 0 
-    puts "#{number} SECOND(S)!"
-    number-=1
+  puts "#{number} SECOND(S)!"
+  number-=1
   end
   return "HAPPY NEW YEAR!"
 end
